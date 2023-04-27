658da24da900dcee4db76f0fb99fae030bf00633
jzh <jiangzenghui005@163.com>
2023-04-27 17:18:50 +0800

    支持外部设置网络图片加载。

--- Sources/SKPhotoBrowserSwiftUI/SKPhotoBrowserSwiftUI.swift
+++ Sources/SKPhotoBrowserSwiftUI/SKPhotoBrowserSwiftUI.swift
@@ -25,10 +25,9 @@ public struct SKPhotoBrowserSwiftUI: UIViewControllerRepresentable {
     public let deleteButtonInsets: UIEdgeInsets?
     public let counterLocaton: CounterLocation?
     public let counterExtraMarginY: CGFloat?
+    public var loadImageBlock: ((URL, @escaping (UIImage?, Error?) -> Void) -> Void)?
     
-    
-    
-    public init(images: Binding<[UIImage]> = .constant([]), urls: Binding<[String]> = .constant([]), page: Int = 0, closeImage: UIImage? = nil, deleteImage: UIImage? = nil, displayCloseButton: Bool = false, enableSingleTapDismiss: Bool = true, displayDeleteButton: Bool? = nil, actionBackgroundColor: UIColor? = nil, actionTextColor: UIColor? = nil, actionFont: UIFont? = nil, actionTextShadowColor: UIColor? = nil, closeButtonPadding: CGPoint? = nil, closeButtonInsets: UIEdgeInsets? = nil, deleteButtonPadding: CGPoint? = nil, deleteButtonInsets: UIEdgeInsets? = nil, counterLocaton: CounterLocation? = nil, counterExtraMarginY: CGFloat? = nil) {
+    public init(images: Binding<[UIImage]> = .constant([]), urls: Binding<[String]> = .constant([]), page: Int = 0, closeImage: UIImage? = nil, deleteImage: UIImage? = nil, displayCloseButton: Bool = false, enableSingleTapDismiss: Bool = true, displayDeleteButton: Bool? = nil, actionBackgroundColor: UIColor? = nil, actionTextColor: UIColor? = nil, actionFont: UIFont? = nil, actionTextShadowColor: UIColor? = nil, closeButtonPadding: CGPoint? = nil, closeButtonInsets: UIEdgeInsets? = nil, deleteButtonPadding: CGPoint? = nil, deleteButtonInsets: UIEdgeInsets? = nil, counterLocaton: CounterLocation? = nil, counterExtraMarginY: CGFloat? = nil, loadImageBlock: ((URL, @escaping (UIImage?, Error?) -> Void) -> Void)? = nil) {
         self._images = images
         self._urls = urls
         self.page = page
@@ -47,6 +46,7 @@ public struct SKPhotoBrowserSwiftUI: UIViewControllerRepresentable {
         self.deleteButtonInsets = deleteButtonInsets
         self.counterLocaton = counterLocaton
         self.counterExtraMarginY = counterExtraMarginY
+        self.loadImageBlock = loadImageBlock
     }
     
     public typealias UIViewControllerType = SKPhotoBrowser
@@ -96,7 +96,11 @@ public struct SKPhotoBrowserSwiftUI: UIViewControllerRepresentable {
             photos = images.map({ SKPhoto.photoWithImage($0) })
         }
         else if !urls.isEmpty {
-            photos = urls.map({ SKPhoto.photoWithImageURL($0) })
+            photos = urls.map{
+                let photo = SKPhoto.photoWithImageURL($0)
+                photo.loadImageBlock = loadImageBlock
+                return photo
+            }
         }
         let browser = SKPhotoBrowser(photos: photos, initialPageIndex: page)
         browser.delegate = context.coordinator
